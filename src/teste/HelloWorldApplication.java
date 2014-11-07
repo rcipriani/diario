f58043d40b658c9e11ff64146162255b3435ab31
Rodrigo Cipriani <chopsss@gmail.com>
2014-11-07 11:17:56 -0200

    RestFul + Json funcionando

---
+++ src/teste/HelloWorldApplication.java
@@ -0,0 +1,28 @@
+package teste;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.core.Application;
+ 
+import teste.HelloWorld;
+ 
+public class HelloWorldApplication extends Application
+{
+    private Set<Object> singletons = new HashSet();
+    private Set<Class<?>> empty = new HashSet();
+ 
+    public HelloWorldApplication() {
+        // ADD YOUR RESTFUL RESOURCES HERE
+        this.singletons.add(new HelloWorld());
+    }
+ 
+    public Set<Class<?>> getClasses()
+    {
+        return this.empty;
+    }
+ 
+    public Set<Object> getSingletons()
+    {
+        return this.singletons;
+    }
+}
---
+++ src/teste/TesteList.java
@@ -0,0 +1,28 @@
+package teste;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+ 
+ 
+@XmlRootElement(name="listing")
+public class TesteList
+{
+    private List<Teste> testes;
+ 
+    public TesteList()
+    {
+    }
+ 
+    public TesteList(List<Teste> testes)
+    {
+        this.testes = testes;
+    }
+ 
+    @XmlElement(name="testes")
+    public List<Teste> getTestes()
+    {
+        return testes;
+    }
+}
